8a0e506685f9ab9a01d64ae347d52057f9d5d0e5
Andrew Muto <amuto77@gmail.com>
2020-07-27 13:55:28 -0400

    Routes set up

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+import Home from './components/Home'
+import Navigation from './components/Navigation'
+import Login from './components/Login'
+import Register from './components/Register'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>African Marketplace</h1>
+      <Navigation />
+      <Switch>
+        <Route path='/'>
+          <Home />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/register'>
+          <Register />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
