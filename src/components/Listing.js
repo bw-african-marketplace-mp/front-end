2e9953471986a2889ef5be19ff19ff88045b0b0b
Andrew Muto <amuto77@gmail.com>
2020-07-29 18:10:58 -0400

    list card structure

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -5,19 +5,25 @@ const Listing = props => {
     return(
         <div className='item-card'>
             <div className='description'>
-            Product: {props.item.product_name};
-            </div>
-            <div className='description'>
-            Description: {props.item.product_description};
-            </div>
-            <div className='description'>
-            Quantity: {props.item.product_quantity};
+            Product Name: {props.item.product_name};
             </div>
             <div className='description'>
             Price: {props.item.product_price}
             </div>
             <div className='description'>
+            Quantity: {props.item.product_quantity};
+            </div>
+            <div className='description'>
+            Category: {props.item.product_category}
+            </div>
+            <div className='description'>
+            Description: {props.item.product_description};
+            </div>
+            <div className='description'>
             Country: {props.item.country}
+            </div> 
+             <div className='description'>
+            Market Name: {props.item.market_name}
             </div> 
         </div>
     )
