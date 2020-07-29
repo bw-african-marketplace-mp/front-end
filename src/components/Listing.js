7f98cc2be6ef6b4c4a7114fa0fd957232df51122
Andrew Muto <amuto77@gmail.com>
2020-07-29 17:00:31 -0400

    completed item cards

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -3,7 +3,20 @@ import React from 'react'
 const Listing = props => {
     console.log(props)
     return(
-        <div>Placeholder text {props.item.id}</div>
+        <div className='item-card'>
+            <div className='description'>
+            Product: {props.item.product_name};
+            </div>
+            <div className='description'>
+            Description: {props.item.product_description};
+            </div>
+            <div className='description'>
+            Quantity: {props.item.product_quantity};
+            </div>
+            <div className='description'>
+            Price: {props.item.product_price}
+            </div> 
+        </div>
     )
 }
 
