0f78c61043dd70f2ff5ad2b8f46193b9d631a1cf
Andrew Muto <amuto77@gmail.com>
2020-07-30 17:59:45 -0400

    Successful login

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import '../App.css';
 
-function Navigation () {
+function Navigation (props) {
     const logout = function(){
         localStorage.removeItem('username')
         localStorage.removeItem('token')
+        props.setLoggedIn(false);
     }
     return (
         <>
-        {localStorage.getItem('token') 
-        ? <div className='nav'><NavLink to='/' className='link'>Home</NavLink> <NavLink to='/login' className='link' onClick={logout}>Log Out</NavLink></div>
-        : <div className='nav'> <NavLink to='/register' className='link'>Register</NavLink> <NavLink to='/login' className='link'>Log In</NavLink></div>
+        {props.loggedIn 
+        ? (<div className='nav'><NavLink to='/' className='link'>Home</NavLink> <NavLink to='/login' className='link' onClick={logout}>Log Out</NavLink></div>)
+        : (<div className='nav'> <NavLink to='/register' className='link'>Register</NavLink> <NavLink to='/login' className='link'>Log In</NavLink></div>)
         }
         </>
     )
